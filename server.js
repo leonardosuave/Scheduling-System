2b8001c8b48880236f97bb4ab64a89f8287f96b9
Leonardo Suave <leonardo.suave15@hotmail.com>
2022-09-05 17:53:19 -0300

    Mask and model

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({extended:false}));
 app.use(express.json())
 
 app.use(express.static('public'))
-app.set('view', path.resolve(__dirname, 'src', 'Views'))
+app.set('views', path.resolve(__dirname, 'src', 'views'))
 app.set('view engine', 'ejs')
 
 mongoose.connect('mongodb://localhost:27017/agendamento', {useNewUrlParser: true, useUnifiedTopology: true})
